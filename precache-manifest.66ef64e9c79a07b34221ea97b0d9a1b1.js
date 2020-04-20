53444eb9217b3772916857de1aeffb9a37f59002
ChinmayPatil <32126212+Chinmay-Patil@users.noreply.github.com>
2020-04-20 01:30:39 -0500

    Updates

---
+++ precache-manifest.66ef64e9c79a07b34221ea97b0d9a1b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b0835b8f4efbfc3d8d71e06f0d46e8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bb9a9c5e94d06bab517e",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f10e04315f6645c86b4d",
+    "url": "/portfolio/static/css/main.87374b8c.chunk.css"
+  },
+  {
+    "revision": "bb9a9c5e94d06bab517e",
+    "url": "/portfolio/static/js/2.dde373ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.dde373ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10e04315f6645c86b4d",
+    "url": "/portfolio/static/js/main.d238e6fc.chunk.js"
+  },
+  {
+    "revision": "167df95dcffba2b4b46a",
+    "url": "/portfolio/static/js/runtime-main.ea406fff.js"
+  },
+  {
+    "revision": "ef118a95694c69601fda72e794de9778",
+    "url": "/portfolio/static/media/CR.ef118a95.jpg"
+  },
+  {
+    "revision": "ae5318c873caa69721946ac34c0b2835",
+    "url": "/portfolio/static/media/PR.ae5318c8.jpg"
+  },
+  {
+    "revision": "8438217b322bb143ddb0afbc1d3785de",
+    "url": "/portfolio/static/media/RE.8438217b.jpg"
+  },
+  {
+    "revision": "35440e7bbbf33a039af94070c9a674a5",
+    "url": "/portfolio/static/media/SA.35440e7b.jpg"
+  },
+  {
+    "revision": "feb678d51f2ea40246f3b8733550526f",
+    "url": "/portfolio/static/media/VS.feb678d5.jpg"
+  }
+]);
